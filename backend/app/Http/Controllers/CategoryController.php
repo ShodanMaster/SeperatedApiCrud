3d8d00926455be503e2f64da5b7ff52111345b65
ShodanMaster <akshaykrishnan922@gmail.com>
2025-08-22 15:43:06 +0530

    Front Page Setup

--- backend/app/Http/Controllers/CategoryController.php
+++ backend/app/Http/Controllers/CategoryController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class CategoryController extends Controller
 {
     public function index(){
-
+        return response()->json([
+            'status' => 200,
+            'message' => 'Category fetched successfully',
+            'data' => []
+        ]);
     }
 }
