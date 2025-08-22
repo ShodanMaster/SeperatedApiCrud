ccf051fdc8336a6724b856a82bd20fdf2796c85a
ShodanMaster <akshaykrishnan922@gmail.com>
2025-08-22 16:30:21 +0530

    Create Product

--- backend/app/Http/Controllers/ProductController.php
+++ backend/app/Http/Controllers/ProductController.php
@@ -2,9 +2,57 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 class ProductController extends Controller
 {
-    //
+    public function index(){
+        $products = Product::all();
+        $products = $products->map(function($product){
+            return [
+                'id' => $product->id,
+                'name' => $product->name,
+                'category' => $product->category ? $product->category->name : null,
+            ];
+        });
+        return response()->json([
+            'status' => 200,
+            'message' => 'Product fetched successfully',
+            'data' => $products
+        ]);
+    }
+
+    public function store(Request $request){
+        try{
+            $request->validate([
+                'category_id' => 'required|exists:categories,id',
+                'name' => 'required|unique:products',
+            ]);
+
+            $product = new Product();
+            $product->name = $request->name;
+            $product->category_id = $request->category_id;
+            $product->save();
+
+            return response()->json([
+                'status' => 201,
+                'message' => 'Product created successfully',
+            ]);
+        }catch(ValidationException $e){
+            return response()->json([
+                'status' => 422,
+                'message' => 'Validation failed',
+                'errors' => $e->errors()
+            ], 422);
+        }catch(Exception $e){
+            return response()->json([
+                'status' => 500,
+                'message' => 'An error occurred while creating the product',
+                'error' => $e->getMessage()
+            ], 500);
+        }
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,3 +10,4 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::resource('category', CategoryController::class);
+Route::resource('product', ProductController::class);
